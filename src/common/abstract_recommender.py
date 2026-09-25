8f5f0069fabecc8a69d59dca233d6c63fd15f822
ben0222 <1227016120@qq.com>
2026-09-25 16:21:49 +0800

    exp: add modality switches for VBPR

--- src/common/abstract_recommender.py
+++ src/common/abstract_recommender.py
@@ -93,10 +93,10 @@ class GeneralRecommender(AbstractRecommender):
             # if file exist?
             v_feat_file_path = os.path.join(dataset_path, config['vision_feature_file'])
             t_feat_file_path = os.path.join(dataset_path, config['text_feature_file'])
-            if os.path.isfile(v_feat_file_path):
+            if config['use_visual'] is not False and os.path.isfile(v_feat_file_path):
                 self.v_feat = torch.from_numpy(np.load(v_feat_file_path, allow_pickle=True)).type(torch.FloatTensor).to(
                     self.device)
-            if os.path.isfile(t_feat_file_path):
+            if config['use_textual'] is not False and os.path.isfile(t_feat_file_path):
                 self.t_feat = torch.from_numpy(np.load(t_feat_file_path, allow_pickle=True)).type(torch.FloatTensor).to(
                     self.device)
 
